0ab16f5b1da966cfd3019933ff03f459dde814d1
1712722@student.hcmus.edu.vn <1712722@student.hcmus.edu.vn>
2020-11-08 20:27:01 +0700

    first

---
+++ models/bookModel.js
@@ -0,0 +1,22 @@
+exports.list = () => {
+    return [
+        {
+            id: 1,
+            title: 'Chị Dậu',
+            cover: 'https://picsum.photos/200/300',
+            basePrice: 18000
+        },
+        {
+            id: 2,
+            title: 'Harry Potter',
+            cover: 'https://picsum.photos/200/300',
+            basePrice: 180000
+        },
+        {
+            id: 3,
+            title: 'Tiếng Việt 1 tập 1',
+            cover: 'https://picsum.photos/200/300',
+            basePrice: 10000
+        },
+    ]
+}
